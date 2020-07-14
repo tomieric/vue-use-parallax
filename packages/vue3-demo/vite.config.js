50a5174a6eef21a907f65632ead30249f7cbedc4
tomieric <tomieric@gmail.com>
2020-07-14 20:03:18 +0800

    ci(ci):

--- packages/vue3-demo/vite.config.js
+++ packages/vue3-demo/vite.config.js
@@ -2,5 +2,6 @@ const resolve = require('path').resolve
 
 export default {
   base: process.env.NODE_ENV === 'production' ? '/vue-use-parallax/' : '/',
-  outDir: resolve(__dirname, '../../dist')
+  outDir: resolve(__dirname, '../../dist'),
+  assetsDir: 'assets'
 }
